5a188fedadb1ba4e41a7aeeeb9af5449d63fc7ba
BlankDiploma <BlankDiploma@gmail.com>
2014-02-06 23:08:19 -0800

    remove autogen files from repository

--- Projects/Evilization/Evilization/Autogen/ParseTableDict.h
+++
@@ -1,5 +0,0 @@
-#include "stdafx.h"
-
-void PopulateParseTableDict(ParseTableNameHash* phtNames);
-void PopulatePolyTableNameHash(PolyTableNameHash* phtPolyNames);
-void PopulateObjSizeHash(ObjSizeHash* phtObjSize);
